432ede6d194ee64a995f80aa50ee7a867f18f513
12Sanchi <sanchigupta68@gmail.com>
2024-06-14 05:14:30 +0530

    files updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,21 +5,20 @@ import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 
 const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
 
-    const appRouter = createBrowserRouter([
-        {
-            path: "/"
-            element: <Body />,
-        },
-        {
-            path:"/browse",
-            element:<Browse />,
-        };
-    ]);
   return (
     <div>
-      <Login />
-      <Browse />
+      <RouterProvider router={appRouter} />
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/51c1d7f7-3179-4a55-93d9-704722898999/be90e543-c951-40d0-9ef5-e067f3e33d16/IN-en-20240610-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0">
+        <input type="text" placeholder="Email Addess" classname="p-2 my-2" />
+        <input type="password" placeholder="Password" classname="p-2 my-2" />
+        <button className="p-4 m-4">Sign In</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
